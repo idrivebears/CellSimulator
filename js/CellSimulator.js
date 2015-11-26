cec3b434609ee4b4a43b588fb18505806a642294
Alex Walls <idrivebears@gmail.com>
2015-11-26 07:57:20 -0600

    modified main file, setting up for great dissapointment

--- js/CellSimulator.js
+++ js/CellSimulator.js
@@ -17,31 +17,25 @@
     function preload() {
 
         game.load.image('background', 'assets/bg.png');
-        game.load.image('ground', 'assets/platform.png');
-        game.load.image('star', 'assets/star.png');
         game.load.spritesheet('commoncell', 'assets/CommonCell2_Sprite.png', 64, 64);
         game.load.spritesheet('whitebloodcell', 'assets/CommonCell_Sprite.png', 64, 64);
     }
 
-    var player;
-    var platforms;
     var cursors;
     var background;
 
     var cells;
-    var score = 0;
-    var scoreText;
 
     function create() {
         // Enable aracde physics for game
         game.physics.startSystem(Phaser.Physics.ARCADE);
+        
         // Add background sprite to game instance
         background = game.add.sprite(0, 0, 'background');
 
-
-
         // Create some cells
         cells = game.add.group();
+
         //  We will enable physics for any star that is created in this group
         cells.enableBody = true;
 
@@ -65,15 +59,13 @@
             tempCell.body.width = 35;
         }
 
-        scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
-
         cursors = game.input.keyboard.createCursorKeys();
 
     }
 
     function update() {
 
-        //Add collision between cells and platforms
+        //Add collision between cells and other cells
         game.physics.arcade.collide(cells, cells);
         //game.physics.arcade.overlap(cells, cells, cellCollision, null, this);
 
@@ -93,10 +85,8 @@
         {
             //camera movement
         }
-        /*
-        for(cell in cells) {
-            cell.body.velocity.x += 1.0;
-            cell.body.velocity.y += 1.0;
-        }*/
+
+        //Call cell update function
+        //Call white blood cell update function
     }
 })();
---
+++ js/CommonCell.js
@@ -0,0 +1,17 @@
+/*
+    CommonCell class
+*/
+
+var CommonCell = function(x, y, game) {
+    this.x = x;
+    this.y = y;
+    this.game = game;
+};
+
+CommonCell.prototype.move = function() {
+    
+};
+
+CommonCell.prototype.update = function() {
+    
+};
